a87b51c08257e19742d279e7ffbb4ec1079cae9b
jonasjerebko <mialicea55@gmail.com>
2019-04-03 01:50:26 -0400

    Add files via upload

--- assets/js/redirect.js
+++ assets/js/redirect.js
@@ -1,6 +1,6 @@
 var urls = new Array();
-urls[0] = "https://www.fartshd.ml/fartss.html";  
-urls[1] = "https://www.fartshd.ml/2farts.html";
+urls[0] = "https://fartshd.ml/fartss.html";  
+urls[1] = "https://fartshd.ml/2farts.html";
 
 var random = Math.floor(Math.random()*urls.length);
 
