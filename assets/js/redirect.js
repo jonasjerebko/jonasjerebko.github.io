afbf37f54b7dc603c2f8feea651159744177c0b4
jonasjerebko <mialicea55@gmail.com>
2019-04-11 02:21:04 -0400

    Delete redirect.js

--- assets/js/redirect.js
+++
@@ -1,7 +0,0 @@
-var urls = new Array();
-urls[0] = "//www.fartshd.ml/fartss.html";  
-urls[1] = "//www.fartshd.ml/2farts.html";
-
-var random = Math.floor(Math.random()*urls.length);
-
-window.location = urls[random];
